72b03ee6e5b8a46b3d5555193d956895707a8139
hamed21q <hasanihamed220@gmail.com>
2023-12-22 14:32:25 +0330

    cypress end-to-end test

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
+import {saveTodo} from "../lib/service";
 
 
 export default class TodoApp extends Component {
@@ -10,8 +11,26 @@ export default class TodoApp extends Component {
     super(props)
 
     this.state = {
+      currentTodo: '',
       todos: []
     }
+    this.handleNewTodoChange = this.handleNewTodoChange.bind(this)
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this)
+  }
+
+  handleNewTodoChange (evt){
+    this.setState({currentTodo: evt.target.value})
+  }
+
+  handleTodoSubmit (evt) {
+    evt.preventDefault()
+    const newTodo = {name: this.state.currentTodo, isComplete: false}
+    saveTodo(newTodo)
+        .then(({data}) => this.setState({
+          todos: this.state.todos.concat(data),
+          currentTodo: ''
+        }))
+        .catch(() => this.setState({error: true}))
   }
 
 
@@ -22,10 +41,15 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
-            <TodoForm />
+            {this.state.error ? <span className='error'>Oh no!</span> : null}
+            <TodoForm
+                currentTodo={this.state.currentTodo}
+                handleNewTodoChange={this.handleNewTodoChange}
+                handleTodoSubmit={this.handleTodoSubmit}
+            />
           </header>
           <section className="main">
-            <TodoList todos={this.state.todos} />
+            <TodoList todos={this.state.todos}/>
           </section>
           <Footer />
         </div>
